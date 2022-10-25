904c7c8887c69841775a80e218d17dc95e54585d
Jordan Lima <60404264+Jordan-Lima@users.noreply.github.com>
2022-10-24 21:49:42 -0300

    Add new components
    In these new commits, the React carousel component and the footer component were added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import Header from './components/Header'
+import Carrossel from './components/Carrossel'
+import Footer from './components/Footer'
+import MapSite from './components/MapSite'
 
 function App() {
   return (
     <div>
       <Header />
+      <Carrossel />
+      <MapSite />
+      <Footer />
     </div>
   )
 }
